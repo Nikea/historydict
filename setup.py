095ee9a0de1f2332d98eca912103d13e406f6d9c
Thomas A Caswell <tcaswell@gmail.com>
2015-06-05 19:24:29 -0400

    BLD: added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='history',
+      version='0.1.0',
+      author='Brookhaven National Laboratory',
+      py_modules=['history'],
+      description='A persistent dictionary with history backed by sqlite',
+      url='http://github.com/Nikea/history',
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      )
