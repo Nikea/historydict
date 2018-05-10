0bd744fe35f963b9a6c6a314c6484246048d27a4
danielballan <daniel.b.allan@gmail.com>
2018-05-10 08:09:47 -0400

    REL: 1.2.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='historydict',
-      version='1.2.1',
+      version='1.2.3',
       author='Brookhaven National Laboratory',
       py_modules=['historydict'],
       description='A persistent dictionary with history backed by sqlite',
