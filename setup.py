3b2a13717b266d67d426a636935312c296e5e456
Thomas A Caswell <tcaswell@bnl.gov>
2023-07-20 11:08:49 -0400

    BLD: add license to setup.py metadata

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(name='historydict',
       description='A persistent dictionary with history backed by sqlite',
       url='http://github.com/Nikea/historydict',
       platforms='Cross platform (Linux, Mac OSX, Windows)',
+      license_files=('LICENSE.',),
       )
